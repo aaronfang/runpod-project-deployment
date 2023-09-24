3781bc9de29e0435d673c55933a8ce1d9c591d8e
aaronfang <aaronfang@tencent.com>
2023-09-24 22:15:02 +0800

    update

--- launcher.sh
+++ launcher.sh
@@ -1,14 +1,19 @@
 #!/bin/bash
 
 #### init ####
-apt-get update
-apt-get install -y unzip
-apt-get install lsof
-apt-get install dos2unix
-apt-get install -y git
-apt-get install -y aria2
+if [ ! -d "runpod-project-deployment" ]; then
+    echo "clone repo"
+    git clone https://github.com/aaronfang/runpod-project-deployment.git
+    
+    apt-get update
+    apt-get install -y unzip
+    apt-get install lsof
+    apt-get install dos2unix
+    apt-get install -y git
+    apt-get install -y aria2
 
-pip install requests tqdm gdown
+    pip install requests tqdm gdown
+fi
 
-#### launch gui ####
+cd runpod-project-deployment || exit
 python main.py
